69ff0b6ae5f4263905c7f670436787a5fa98193d
b3rnoulli <rafal.kowalski@ifj.edu.pl>
2017-05-30 22:00:46 +0200

    path as env variable in mfdfa_window_suirrogate script

--- impact of electronic trading on financal markets/mfdfa_window_surrogate.m
+++ impact of electronic trading on financal markets/mfdfa_window_surrogate.m
@@ -1,8 +1,8 @@
 % index name, start year, end year
 clear
 indexes = {
-    'SP500-removed', '01-Jan-1950', '31-Dec-2016';
-    'NASDAQ',        '01-Jan-1950', '31-Dec-2016';
+    'SP500-removed',         '01-Jan-1950', '31-Dec-2016';
+    'NASDAQ-removed',        '01-Jan-1950', '31-Dec-2016';
     };
 
 frame_size = 20;
@@ -13,7 +13,7 @@ frame_step_type = 'MONTH';
 surrogate_count = 10;
 
 for i=1:length(indexes(:,1))
-    path = ['/Users/b3rnoulli/Development/Matlab workspace/empirical data/',indexes{i,1},'/spectrum/window/surrogate/'];
+    path = [get_root_path(),'/financial-analysis/empirical data/',indexes{i,1},'/spectrum/window/surrogate/'];
     data = load(indexes{i,1});
     
     start_index = find_index(data.date,indexes{i,2});
@@ -23,27 +23,26 @@ for i=1:length(indexes(:,1))
         fprintf('[surrogate_mfdfa_script] : Calculating MFDFA for surrogate index %s date scope %s to %s\n', indexes{i,1},...
             datestr(data.date(start_index)), datestr(data.date(end_index)));
         
-        fourier_surrogate_file_name = [indexes{i,1},'-fourier-surrogate-',datestr(data.date(start_index)),...
-            '-',datestr(data.date(end_index))];
+        fourier_surrogate_file_name = [indexes{i,1},'-fourier-surrogate-',datestr(data.date(start_index),'yyyy-mm-dd'),...
+            '-',datestr(data.date(end_index),'yyyy-mm-dd')];
         
-        shuffled_surrogate_file_name = [indexes{i,1},'-shuffled-surrogate-',datestr(data.date(start_index)),...
-            '-',datestr(data.date(end_index))];
+        shuffled_surrogate_file_name = [indexes{i,1},'-shuffled-surrogate-',datestr(data.date(start_index),'yyyy-mm-dd'),...
+            '-',datestr(data.date(end_index),'yyyy-mm-dd')];
         
         
         fourier_surrogate_data = load(fourier_surrogate_file_name);
         shuffled_surrogate_data = load(shuffled_surrogate_file_name);
         
-        parfor j=1:surrogate_count
+        for j=1:surrogate_count
             fourier_surrogate_mfdfa_matrix(j) = MFDFA(fourier_surrogate_data.fourier_surrogate_matrix(j,:),' not_used', false);
-            shuffled_surrogate_mfdfa_matrix(j) = MFDFA(shuffled_surrogate_data.shuffled_surrogate_matrix(j,:),'not_used', false);
-            
+            shuffled_surrogate_mfdfa_matrix(j) = MFDFA(shuffled_surrogate_data.shuffled_surrogate_matrix(j,:),'not_used', false);       
         end
         
-        fourier_surrogate_mfdfa_file_name =  [indexes{i,1},'-fourier-surrogate-spectrum-',datestr(data.date(start_index)),...
-            '-',datestr(data.date(end_index))];
+        fourier_surrogate_mfdfa_file_name =  [indexes{i,1},'-fourier-surrogate-spectrum-',datestr(data.date(start_index),'yyyy-mm-dd'),...
+            '-',datestr(data.date(end_index),'yyyy-mm-dd')];
         
-        shuffled_surrogate_mfdfa_file_name =  [indexes{i,1},'-shuffled-surrogate-spectrum-',datestr(data.date(start_index)),...
-            '-',datestr(data.date(end_index))];
+        shuffled_surrogate_mfdfa_file_name =  [indexes{i,1},'-shuffled-surrogate-spectrum-',datestr(data.date(start_index),'yyyy-mm-dd'),...
+            '-',datestr(data.date(end_index),'yyyy-mm-dd')];
         
         save([path,fourier_surrogate_mfdfa_file_name],'fourier_surrogate_mfdfa_matrix');
         save([path,shuffled_surrogate_mfdfa_file_name],'shuffled_surrogate_mfdfa_matrix');
