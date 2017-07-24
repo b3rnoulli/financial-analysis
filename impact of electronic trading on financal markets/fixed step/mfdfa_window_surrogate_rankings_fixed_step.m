2c9bf7a4fc38fbce90128b90cda3a91a86df6901
Rafal Kowalski <b3rnoulli@MacBook-Pro-Rafal.local>
2017-07-24 02:22:45 +0200

    added fixed step implementation for rankings mfdfa

---
+++ impact of electronic trading on financal markets/fixed step/mfdfa_window_surrogate_rankings_fixed_step.m
@@ -0,0 +1,44 @@
+% index name, start year, end year
+clear
+clc
+indexes = {
+    'SP500-removed',         '01-Jan-1950', '31-Dec-2016';
+    'DJIA',                  '01-Jan-1950', '31-Dec-2016';
+    };
+
+frame_size = 5000;
+frame_step_size = 20;
+
+surrogate_count = 100;
+
+for i=1:length(indexes(:,1))
+    path = [get_root_path(),'/financial-analysis/empirical data/',indexes{i,1},'/spectrum/window/surrogate/'];
+    data = load(indexes{i,1});
+    
+    start_index = 1;
+    end_index = frame_size;
+    
+    while end_index < find_index(data.date,indexes{i,3})
+        fprintf('[surrogate_mfdfa_script] : Calculating MFDFA for surrogate index %s date scope %s to %s\n', indexes{i,1},...
+            datestr(data.date(start_index)), datestr(data.date(end_index)));
+        
+        rankings_surrogate_file_name = [indexes{i,1},'-rankings-surrogate-',datestr(data.date(start_index),'yyyy-mm-dd'),...
+            '-',datestr(data.date(end_index),'yyyy-mm-dd')];
+        
+        rankings_surrogate_data = load(rankings_surrogate_file_name);
+        
+        parfor j=1:surrogate_count
+            rankings_surrogate_mfdfa_matrix(j) = MFDFA(rankings_surrogate_data.rankings_surrogate_matrix(j,:),' not_used', false);
+        end
+        
+        rankings_surrogate_mfdfa_file_name =  [indexes{i,1},'-rankings-surrogate-spectrum-',datestr(data.date(start_index),'yyyy-mm-dd'),...
+            '-',datestr(data.date(end_index),'yyyy-mm-dd')];
+        
+        save([path,rankings_surrogate_mfdfa_file_name],'rankings_surrogate_mfdfa_matrix');
+        
+        clear rankings_surrogate_mfdfa_matrix
+        
+        start_index = start_index + frame_step_size;
+        end_index = end_index + frame_step_size;
+    end
+end
